d4463f5bd124a984f8010c9438aea39fd07e1a45
chinyere <pchukwu757@example.com>
2022-10-18 03:58:37 -0700

    0

--- 2-number.c
+++ 2-number.c
@@ -31,63 +31,36 @@ return (sum);
 int print_rev(va_list ap, params_t *params)
 {
 int len, sum = 0;
-  
-  char *str = va_arg(ap, char *);
-  
-  (void)params;
-  
+char *str = va_arg(ap, char *);
+(void)params;
 
-  
-  if (str)
-    
-    {
-      
-      for (len = 0; *str; str++)
-	
-	len++;
-      
-      str--;
-      
-      for (; len > 0; len--, str--)
-	
-	sum += _putchar(*str);
-      
-    }
-  
-  return (sum);
-  
+if (str)
+{
+for (len = 0; *str; str++)
+len++;
+str--;
+for (; len > 0; len--, str--)
+sum += _putchar(*str);
+}
+return (sum);
 }
 
-
-
 /**
-
- * print_rot13 - prints string in rot13
-
- * @ap: string
-
- * @params: the parameters struct
-
- *
-
- * Return: number bytes printed
-
- */
-
+* print_rot13 - prints string in rot13
+* @ap: string
+* @params: the parameters struct
+*
+* Return: number bytes printed
+*/
 int print_rot13(va_list ap, params_t *params)
-  
 {
-  
-  int i, index;
-  
-  int count = 0;
-  
-  char arr[] =
-    
-    "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+int i, index;
+int count = 0;
+char arr[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
 char *a = va_arg(ap, char *);
 (void)params;
-  
+
 i = 0;
 index = 0;
 while (a[i])
@@ -102,5 +75,5 @@ else
 count += _putchar(a[i]);
 i++;
 }
-return (count); 
+return (count);
 }
